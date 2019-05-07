d70df810b7937cc365f3682f865c1166f8f15f37
Earving8 <Earving8@users.noreply.github.com>
2019-05-07 16:00:52 -0500

    added tx

---
+++ UART_Controller.srcs/sources_1/new/uart_tx.vhd
@@ -0,0 +1,110 @@
+----------------------------------------------------------------------------------
+-- Company: KU 443
+-- Engineer: Nathan MF'in Nichols
+-- 
+-- Create Date: 04/27/2019 03:38:33 PM
+-- Design Name: UART_TX
+
+-- Module Name: top - Behavioral
+-- Project Name: 443 Final Proj   
+-- Target Devices: NEXYS 4
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: Numeric_std, Math_real, STD_LOGIC_1164
+-- 
+-- Revision: v0.1
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+use IEEE.math_real.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity uart_tx is
+    generic (
+                clkRate: integer := 100000000;
+                baudRate: integer := 9600
+            );
+            
+    port(
+            tx: out std_logic;               
+            tx_busy: out std_logic;                      
+            tx_data: in std_logic_vector(7 downto 0);
+            tx_en: in std_logic;                      
+            clk: in std_logic                   
+        );
+end uart_tx;
+
+architecture Behavior of uart_tx is
+
+    signal start: std_logic;                      
+    signal isBusy: std_logic;                       
+    signal output: std_logic_vector(10 downto 0);   
+    constant rate: integer := integer(ceil(real(clkRate)/(real(2)*real(baudRate))));
+    signal count: integer range 0 to rate;   
+    signal baudClk: std_logic;                 
+    signal index: integer range 0 to 10; 
+    
+begin
+
+    tx_busy <= isBusy;           
+    tx  <= output(index);    
+
+    process(tx_en)
+    begin
+        if (isBusy = '1') then           
+            start <= '0';
+        elsif (rising_edge(tx_en)) then
+            start <= '1';                
+        end if;
+    end process;
+
+    process(baudClk,start)
+    begin
+        if (start = '1') then            
+            index <= 0;              
+            isBusy <= '1';
+        elsif (index = 10) then
+            isBusy <= '0';
+        elsif (rising_edge(baudClk)) then
+            index <= index + 1;
+        end if;
+    end process;
+    
+    process(tx_en)
+    begin
+        if (rising_edge(tx_en)) then     
+            output(0) <= '1'; 
+            output(1) <= '0'; 
+            output(9 downto 2) <= tx_data; 
+            output(10) <= '1';
+        end if;
+    end process;
+
+    process(clk)
+    begin
+        if (rising_edge(clk)) then     
+            if (count < rate) then  
+                count <= count + 1;  
+            else
+                count <= 0;            
+                baudClk <= not baudClk;     
+            end if;
+        end if;
+    end process;
+    
+end Behavior;
